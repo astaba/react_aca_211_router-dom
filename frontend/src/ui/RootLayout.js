e0a3fb5a7d6e9343e0b12dd99ef242a93f0ef7df
Astar <bahouidi99@gmail.com>
2023-08-23 18:32:14 +0100

    UI feedback with useNavigation
    In this case useNavigation must used in the parent Route of the Route which uses the loader.

--- frontend/src/ui/RootLayout.js
+++ frontend/src/ui/RootLayout.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 
 import MainNavigation from '../components/MainNavigation'
 
 const RootLayout = () => {
+  const navigation = useNavigation();
+  console.log(navigation)
+  
   return (
     <React.Fragment>
       <MainNavigation />
       <main>
+      {navigation.state === "loading" && <p>Loading...</p>}
         <Outlet />
       </main>
     </React.Fragment>
